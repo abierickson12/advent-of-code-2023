120256f55f4f76e3813252c8e37962026547666f
Abigail <aerickson36@gatech.edu>
2023-12-08 17:29:17 -0600

    Day 04 - Part 2

--- day04/day04.py
+++ day04/day04.py
@@ -1,6 +1,3 @@
-import re
-
-
 # Extract a list of the winning numbers and a list of the numbers that were scratched off by the elf
 # for each game
 def parse_scratchcards(scratchcards):
@@ -16,25 +13,40 @@ def parse_scratchcards(scratchcards):
 
 # Count the number of winning numbers that were scratched off per game and calculate
 # the score based on the scoring system
-def calculate_card_points(winning_numbers, scratched_numbers):
-    scores = []
+def calculate_card_matches(winning_numbers, scratched_numbers):
+    win_count = []
     rounds = len(winning_numbers)
     for i in range(rounds):
         count = len([j for j in winning_numbers[i] if j in scratched_numbers[i]])
-        if count != 0:
-            # The score is exponential to the count of winning numbers per game
-            scores.append(pow(2, count - 1))
+        win_count.append(count)
 
-    return scores
+    return win_count
 
 
 def main():
     f = open('scratchcards.txt', 'r')
     scratchcards = f.readlines()
 
+    # Part 1
     winning_numbers, scratched_numbers = parse_scratchcards(scratchcards)
-    scores = calculate_card_points(winning_numbers, scratched_numbers)
+    win_count = calculate_card_matches(winning_numbers, scratched_numbers)
+    scores = []
+    for matches in win_count:
+        # The score is exponential to the count of winning numbers per game
+        if matches != 0:
+            scores.append(pow(2, matches - 1))
+
     print("Total points for the elf's pile of scratchcards:", sum(scores))
+
+    # Part 2
+    count = len(winning_numbers)
+    total_cards = [1] * count
+    for i in range(count):
+        first_copies = win_count[i]
+        for j in range(1, first_copies + 1):
+            total_cards[i+j] += total_cards[i]
+
+    print('Total original and copied scratchcards:', sum(total_cards))
 
 
 main()
